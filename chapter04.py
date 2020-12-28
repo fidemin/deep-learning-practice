274800d6b1dbd70b72aeb84390377960a8745274
Yunhong Min <yhmin84@gmail.com>
2020-12-28 23:42:38 +0900

    chapter04 epoch accuracy

--- chapter04.py
+++ chapter04.py
@@ -6,14 +6,19 @@ from core.layernet import TwoLayersNet
 
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
-train_loss_list = []
-
-num_of_iters = 1000
+num_of_iters = 6000
 train_size = x_train.shape[0]
 input_size = x_train.shape[1]
-print('input_size:', input_size)
 batch_size = 100
 learning_rate = 0.1
+
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+
+iter_per_epoch = max(train_size / batch_size, 1)
+print('train size:', train_size, 'input size:', input_size, 'iter_per_epoch:', iter_per_epoch)
+
 network = TwoLayersNet(input_size=input_size, hidden_size=50, output_size=10)
 
 for i in range(num_of_iters):
@@ -30,13 +35,21 @@ for i in range(num_of_iters):
     loss = network.loss(x_batch, t_batch)
     train_loss_list.append(loss)
 
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        train_acc_list.append(train_acc)
+        test_acc = network.accuracy(x_test, t_test)
+        test_acc_list.append(test_acc)
+        print('train acc, test acc | ' + str(train_acc) + ", " + str(test_acc))
+
 
 # 그래프 그리기
-markers = {'train': 'o', 'test': 's'}
-plt.plot(np.arange(len(train_loss_list)), train_loss_list, label='train loss')
-plt.xlabel("loss")
-plt.ylabel("iterations")
-plt.ylim(0, 5.0)
+print(np.arange(len(train_acc_list)))
+plt.plot(np.arange(len(train_acc_list)), train_acc_list, label='train accuracy')
+plt.plot(np.arange(len(test_acc_list)), test_acc_list, label='test accuracy', linestyle='--')
+plt.xlabel("epoch")
+plt.ylabel("accuracy")
+plt.ylim(0, 1.0)
 plt.legend(loc='lower right')
 plt.show()
 
