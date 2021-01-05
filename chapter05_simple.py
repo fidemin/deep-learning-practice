1fa365b9b6b5c5955d4b95e78960c12eaf8e656b
Yunhong Min <yhmin84@gmail.com>
2021-01-06 00:06:02 +0900

    chapter05 mullayer

---
+++ chapter05_simple.py
@@ -0,0 +1,16 @@
+
+
+from core.layer import MulLayer
+
+mul_apple_layer = MulLayer()
+mul_tax_layer = MulLayer()
+
+
+apple_price = mul_apple_layer.forward(100, 2)
+final_price = mul_tax_layer.forward(apple_price, 1.1)
+print(final_price)
+
+dprice = 1
+dapple_price, dtax = mul_tax_layer.backward(dprice)
+dapple, dapple_num = mul_apple_layer.backward(dapple_price)
+print(dapple, dapple_num, dtax)
