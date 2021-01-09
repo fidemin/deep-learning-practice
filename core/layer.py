964cb0a40b492a7c9486dd918839f709f35ea62e
Yunhong Min <yhmin84@gmail.com>
2021-01-09 11:59:26 +0900

    relu layer added

--- core/layer.py
+++ core/layer.py
@@ -24,3 +24,19 @@ class AddLayer:
 
     def backward(self, dout):
         return dout, dout
+
+
+class ReluLayer:
+    def __init__(self):
+        self.x = None
+
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+        return out
+
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+        return dx
