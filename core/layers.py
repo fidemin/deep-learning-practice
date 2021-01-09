deaf16a14c443c94010a907e4f85044c61fc0b07
Yunhong Min <yhmin84@gmail.com>
2021-01-09 19:22:00 +0900

    adds Affine layer

--- core/layers.py
+++ core/layers.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 
 class MulLayer:
     def __init__(self):
@@ -55,3 +57,33 @@ class SigmoidLayer:
         dx = dout * self.out (1.0 - self.out)
         return dx
 
+
+class AffineLayer:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.original_x_shape = None
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        # for tensor
+        self.original_x_shape = x.shape
+        # x의 차원이 2차원 배열 이상이면, (N, ?) 형태의 배열로 reshape 한다.
+        # 즉, 배치의 각 요소가 1차원 배열이 아니면, 1차원 형태로 flatten 한다고 보면 된다.
+        x = x.reshape(x.shape[0], -1)
+
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+
+        return out
+
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        # self.x.T의 dimension은 self.original_x_shape와 동일하다는 것을 인지하자.
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+
+        dx = dx.reshape(*self.original_x_shape)
+        return dx
