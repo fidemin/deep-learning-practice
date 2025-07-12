927c1f36d5d1b906fd9f2d8d3d1c69eb8385e828
Yunhong Min <yunhongmin00@gmail.com>
2025-07-12 15:49:54 +0900

    bandit expectaion per machine

--- deep-learning-from-scratch-4/scripts/run_bandit.py
+++ deep-learning-from-scratch-4/scripts/run_bandit.py
@@ -1,9 +1,22 @@
+import numpy as np
+
 from src.bandit.bandit import Bandit
 
 if __name__ == "__main__":
-    num = 3
+    num = 10
     bandit = Bandit(num)
 
-    for i in range(num):
-        for _ in range(10):
-            print(f"slot machine {i+1}: {bandit.play(i)}")
+    Qs = np.zeros(num)  # expectation for machine
+    ns = np.zeros(num)  # number of plays for machine
+
+    number_of_plays = 100
+
+    for i in range(number_of_plays):
+        machine = np.random.randint(0, num)  # 0 ~ num-1
+        reward = bandit.play(machine)
+
+        ns[machine] += 1
+        Qs[machine] = Qs[machine] + (reward + Qs[machine]) / ns[machine]
+
+    print(ns)
+    print(Qs)
